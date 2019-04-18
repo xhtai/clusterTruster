d55cb812635b68cc160914d0ff41fb4d2b11d327
Xiao Hui Tai <tai.xiaohui@gmail.com>
2019-04-17 20:51:34 -0400

    add prostate data and documentation

---
+++ R/removedDups.R
@@ -0,0 +1,4 @@
+#' NBIDE data
+#'
+#' @format A data frame with 10296 rows and 6 columns of pairwise comparison data generated using the \code{cartridge3D} package.
+"removedDups"
---
+++ R/singh2002.R
@@ -0,0 +1,12 @@
+#' Prostate data set
+#'
+#' Prostate data from Singh 2002, see
+#' \url{https://stat.ethz.ch/~dettling/bagboost.html}. Code used to produce this
+#' is \code{singh2002 <- cbind(prostate.x, prostate.y); dimnames(singh2002) <-
+#' NULL}.
+#'
+#' @format A 102 x 6034 matrix, with columns 1 through 6033 gene expressions and
+#'   the last column the class label.
+
+#' @source \url{ftp://stat.ethz.ch/Manuscripts/dettling/prostate.rda}
+"singh2002"
